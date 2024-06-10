91f8d9ad66960fedb11a43c3de4e68450c44f52e
James Mugaruro <58384794+Mugaruro@users.noreply.github.com>
2024-06-10 11:32:50 +0300

    Add files via upload

---
+++ DEBZ 2ND PYTHON PROGRAM.py
@@ -0,0 +1,14 @@
+#simple user name print in python
+name="TECLA DEBRA NJERI"
+print("my name is ", name)
+
+location="Nyeri, Kenya"
+print("my location is ",location) 
+
+workplace="pgh, nyeri"
+print("my workplace is ",workplace)
+
+home="nyeri, town"
+print("my home is ",home)
+
+print("i like to sleep for a long time")
